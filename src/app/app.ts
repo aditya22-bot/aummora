c674dc19f0557b1b32809cf072fb844d90c60df3
Aditya Nikhade <adityamikhade22@gmail.com>
2025-11-07 20:29:54 +0530

    setup for a code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+// app.ts
+import { Component } from '@angular/core';
+import { HeadersComponent } from './aumora-dashbord/headers/headers';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+   imports: [RouterOutlet, HeadersComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
-export class App {
-  protected readonly title = signal('aumorra_export');
-}
+export class AppComponent {}
