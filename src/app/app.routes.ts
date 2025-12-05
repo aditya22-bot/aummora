4f70f6a6cfaf776c1cdcd98e1137c6c35fabae43
Aditya Nikhade <adityamikhade22@gmail.com>
2025-12-05 13:27:37 +0530

    hot fix for about section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,14 @@ import { HeadersComponent } from './aumora-dashbord/headers/headers';
 import { About } from './about/about';
 
 export const routes: Routes = [
-     { path: '', component: HeadersComponent },
-  { path: 'about', component: About }
+  {
+    path: '',
+    loadComponent: () =>
+      import('./aumora-dashbord/headers/headers').then(m => m.HeadersComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about').then(m => m.About)
+  }
 ];
-imports: [RouterModule.forRoot(routes)]
-exports: [RouterModule]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 // app.ts
 import { Component } from '@angular/core';
 // import { HeadersComponent } from './aumora-dashbord/headers/headers';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink  } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-   imports: [RouterOutlet],
+   imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
