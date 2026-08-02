5465093d43a7b508151b1c1c25052fb0a2dace53
Aditya Nikhade <adityamikhade22@gmail.com>
2026-08-02 17:21:48 +0530

    new chnages

--- src/app/product-categories/product-categories.ts
+++ src/app/product-categories/product-categories.ts
@@ -15,32 +15,32 @@ export class ProductCategories {
  categories = [
   {
     id: 'vegetables',
-    name: 'Fresh Vegetables',
-    image: 'assets/images/products/vegetables.jpeg',
+    name: 'Basmati Rice',
+    image: 'assets/images/products/basmati_rice.jpeg',
     icon: 'assets/icons/vegetables.svg'
   },
   {
     id: 'spices',
     name: 'Spices',
-    image: 'assets/images/products/indian_spices.jpeg',
+    image: 'assets/images/products/spices.jpeg',
     icon: 'assets/icons/spices.svg'
   },
   {
     id: 'fruits',
     name: 'Fresh Fruits',
-    image: 'assets/images/products/fruits.jpeg',
+    image: 'assets/images/products/fresh_fruit.jpeg',
     icon: 'assets/icons/fruits.svg'
   },
   {
     id: 'dryfruits',
     name: 'Dry Fruits',
-    image: 'assets/images/products/dry_foods.jpeg',
+    image: 'assets/images/products/nuts _and_fruit.jpeg',
     icon: 'assets/icons/nuts.svg'
   },
   {
     id: 'grains',
-    name: 'Grains & Staples',
-    image: 'assets/images/products/grains_cereals.jpeg',
+    name: 'Pulses',
+    image: 'assets/images/products/pulses.jpeg',
     icon: 'assets/icons/grains.svg'
   }
 ];
